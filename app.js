72d9aa243106f0af4df22fc74ee20d3e6c524352
oatratchanon42 <ratchanonj62@nu.ac.th>
2023-08-06 23:19:00 +0700

    SuccessFul-Javascript

---
+++ app.js
@@ -0,0 +1,45 @@
+const currency_one = document.getElementById('currency-one');
+const currency_two = document.getElementById('currency-two');
+
+const amount_one = document.getElementById('amount-one');
+const amount_two = document.getElementById('amount-two');
+
+const rateText = document.getElementById('rate');
+
+const swap = document.getElementById('btn');
+
+
+// Event Currency-one-two 
+
+currency_one.addEventListener('change',calculateMoney);
+currency_two.addEventListener('change',calculateMoney);
+
+amount_one.addEventListener('input',calculateMoney);
+amount_two.addEventListener('input',calculateMoney);
+
+
+
+function calculateMoney(){
+     const one = currency_one.value;
+     const two = currency_two.value;
+        // Import API
+     fetch(`https://api.exchangerate-api.com/v4/latest/${one}`)
+     .then(res=>res.json())
+     .then(data=>{
+        const rate = data.rates[two];
+        rateText.innerText=`1 ${one} = ${rate} ${two}`
+        // ขั้นตอนคำนวณอัตราแลกเปลี่ยน
+        amount_two.value=(amount_one.value*rate).toFixed(2);
+     })
+}
+// สลับสกุลเงิน
+swap.addEventListener('click',()=>{
+    const temp = currency_one.value; // ต้นทาง
+    currency_one.value = currency_two.value;
+    currency_two.value = temp;
+    calculateMoney();
+})
+
+calculateMoney();
+
+
